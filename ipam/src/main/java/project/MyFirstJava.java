0b641ac3f93cd88495b1de233ee3a67c1e243b61
abhishekv1994 <abhishektamuc@gmail.com>
2021-02-07 14:25:50 -0600

    Test2

--- ipam/src/main/java/project/MyFirstJava.java
+++ ipam/src/main/java/project/MyFirstJava.java
@@ -3,15 +3,8 @@ package project;
 public class MyFirstJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hi This is Abhishek v");
-		
-<<<<<<< HEAD:ipam/src/main/java/ipam/MyFirstJava.java
-		System.out.println("Hi This is Abhishek v");
-		
-		System.out.println("Hi This is Abhishek1 v");
-=======
-		System.out.println("Hi This is Abhishek v"); 
->>>>>>> master:ipam/src/main/java/project/MyFirstJava.java
+		System.out.println("Hi This is Abhishek2");
+	
 	}
 
 }
